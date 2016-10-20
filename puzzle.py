bc7a1401755a797054709c50021cb74a1cc073a5
Braybaut <brayan.bautista@seaq.co>
2016-10-20 18:34:25 -0500

    fixed only options 1 or 2

--- puzzle.py
+++ puzzle.py
@@ -127,15 +127,19 @@ class Puzzle:
 g = Puzzle()
 
 typeboard = {'1': g.build_board_manual, '2': g.build_board}
-option=input("1 si quiere el tablero manual o 2 si quiere el tablero automatico:  ")
-try:
-    board=typeboard[option]
-    if option=='2':
-        board(int(input('Ingresa nivel de dificultad: 0 1 2\n2 ''=> Mas alto     0=> Mas bajo\n')))
-    else:
-        board()
-except:
-    print ('opcion incorrecta')
+flag2 = True
+while flag2:
+    option=input("1 si quiere el tablero manual o 2 si quiere el tablero automatico:  ")
+    try:
+        board=typeboard[option]
+        if option=='2':
+            board(int(input('Ingresa nivel de dificultad: 0 1 2\n2 ''=> Mas alto     0=> Mas bajo\n')))
+            break
+        else:
+            board()
+            break
+    except:
+        print ('opcion incorrecta vuelva a intentar')
 #g.build_board(int(input('Ingresa nivel de dificultad: 0 1 2\n2 ''=> Mas alto     0=> Mas bajo\n')))
 g.main_frame()
 init_time = time.time()
